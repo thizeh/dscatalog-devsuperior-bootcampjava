64c949379bfe970d557b77348fecb1e21e792450
thizeh <thiago02_08@hotmail.com>
2022-06-02 16:47:27 -0300

    join between tables

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -3,14 +3,24 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Role implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
-	
+
 	public Role() {
-		
+
 	}
 
 	public Role(Long id, String authority) {
@@ -51,5 +61,4 @@ public class Role implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
-	
 }
